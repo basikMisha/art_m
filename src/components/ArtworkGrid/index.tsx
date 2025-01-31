1c4f8538f7345d6f7913fc1c275e9087b409c0f7
basikMisha <michail.rastigaev@gmail.com>
2025-01-31 10:59:29 +0300

    add artworkgrid component

---
+++ src/components/ArtworkGrid/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ArtworkCard from "@components/Artwork";
+import styled from "styled-components";
+import { Artwork } from "@/api/index";
+
+interface ArtworkGridProps {
+  artworks: Artwork[];
+  onFavorite: (artwork: Artwork) => void;
+  favorites: Artwork[];
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 16px;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const ArtworkGrid: React.FC<ArtworkGridProps> = ({ artworks, onFavorite, favorites }) => {
+  return (
+    <Grid>
+      {artworks.map((artwork) => (
+        <ArtworkCard
+          key={artwork.id}
+          artwork={artwork}
+          onFavorite={onFavorite}
+          isFavorite={favorites.some((fav) => fav.id === artwork.id)}
+        />
+      ))}
+    </Grid>
+  );
+};
+
+export default ArtworkGrid;
