6348ac325e2d35b9a794e6276ef094d9ad007c3c
basikMisha <michail.rastigaev@gmail.com>
2025-01-31 16:46:11 +0300

    add search form functionality

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,40 +1,57 @@
 const API_URL = 'https://api.artic.edu/api/v1';
 
 export interface Artwork {
-    id: number
-    title: string
-    date_display: string
-    artist_display: string
-    image_id: string
-    department_title: string
+  id: number;
+  title: string;
+  date_display: string;
+  artist_display: string;
+  image_id: string;
+  department_title: string;
 }
 
-export const fetchArtworks = async (page: number = 1, searchQuery: string = ""): Promise<{ data: Artwork[], totalPages: number }> => {
-    let url = `https://api.artic.edu/api/v1/artworks?page=${page}&limit=4&fields=id,title,date_display,artist_display,image_id`;
+export const fetchArtworks = async (
+  page: number = 1,
+  searchQuery: string = ''
+): Promise<{ data: Artwork[]; totalPages: number }> => {
+  try {
+    const limit = 4;
+    const searchUrl = `${API_URL}/artworks/search?q=${encodeURIComponent(
+      searchQuery
+    )}&page=${page}&limit=${limit}&fields=id`;
 
-    // Если есть поисковый запрос, добавляем фильтр
-    if (searchQuery.trim() !== "") {
-        url += `&q=${encodeURIComponent(searchQuery)}`;
+    const searchResponse = await fetch(searchUrl);
+    if (!searchResponse.ok) throw new Error('Search request failed');
+
+    const searchData = await searchResponse.json();
+    const artworkIds = searchData.data.map((item: { id: number }) => item.id);
+
+    if (!artworkIds.length) {
+      return { data: [], totalPages: 1 };
     }
+    const artworksUrl = `${
+      API_URL
+    }/artworks?ids=${artworkIds.join(',')}&fields=id,title,date_display,artist_display,image_id`;
+    const artworksResponse = await fetch(artworksUrl);
+    if (!artworksResponse.ok) throw new Error('Artworks request failed');
 
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error("Failed to fetch");
-    }
+    const artworksData = await artworksResponse.json();
 
-    const data = await response.json();
     return {
-        data: data.data, // Список работ
-        totalPages: data.pagination.total_pages || 1, // Общее количество страниц
+      data: artworksData.data,
+      totalPages: searchData.pagination?.total_pages || 1,
     };
+  } catch (error) {
+    console.error(error);
+    return { data: [], totalPages: 1 };
+  }
 };
 
-export const getArtById =  async (id: string) => {
-    const response = await fetch(`${API_URL}/artworks/${id}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch artwork");
-    }
-    const data = await response.json();
-    console.log("API Response:", data); // Лог для проверки API
-    return data.data;
-  };
+export const getArtById = async (id: string) => {
+  const response = await fetch(`${API_URL}/artworks/${id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch artwork');
+  }
+  const data = await response.json();
+  console.log('API Response:', data); // Лог для проверки API
+  return data.data;
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,48 +1,56 @@
-import React, { useState, useEffect} from "react";
-import { Footer } from '@components/Footer'
-import { Header } from '@/components/Header'
-import { Artwork, fetchArtworks } from "@/api/index";
-import ArtworkGrid from "@components/ArtworkGrid";
-import Pagination from "@/components/Pagination";
-import Loader from "@/components/Loader";
-import { useFavorites } from "@/context/FavoritesContext";
-
+import React, { useState, useEffect } from 'react';
+import { Footer } from '@components/Footer';
+import { Header } from '@/components/Header';
+import { Artwork, fetchArtworks } from '@/api/index';
+import ArtworkGrid from '@components/ArtworkGrid';
+import Pagination from '@/components/Pagination';
+import Loader from '@/components/Loader';
+import { useFavorites } from '@/context/FavoritesContext';
+import { useDebounce } from '@/hooks/useDebounce';
+import SearchForm from '@/components/SearchForm';
 const Home: React.FC = () => {
   const [artworks, setArtworks] = useState<Artwork[]>([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
   const { favorites, toggleFavorite } = useFavorites();
+  const [searchQuery, setSearchQuery] = useState('');
+  const debouncedSearchQuery = useDebounce(searchQuery, 300);
 
   useEffect(() => {
     const loadArtworks = async () => {
-        setLoading(true);
-        try {
-            const { data, totalPages } = await fetchArtworks(page);
-            setArtworks(data);
-            setTotalPages(totalPages);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
+      setLoading(true);
+      try {
+        const { data, totalPages } = await fetchArtworks(page, debouncedSearchQuery);
+        setArtworks(data);
+        setTotalPages(totalPages);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     };
+
     loadArtworks();
-}, [page]);
+  }, [page, debouncedSearchQuery]);
 
   return (
     <div>
-        <Header/>
-        
+      <Header />
+      <SearchForm
+        onSearch={(query) => {
+          setSearchQuery(query);
+          setPage(1);
+        }}
+      />
       {loading ? (
-        <Loader/>
+        <Loader />
       ) : (
         <ArtworkGrid artworks={artworks} onFavorite={toggleFavorite} favorites={favorites} />
       )}
       <Pagination currentPage={page} totalPages={totalPages} onPageChange={setPage} />
-      <Footer/>
+      <Footer />
     </div>
-    
   );
 };
 
